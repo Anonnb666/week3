298be6cf8cf41e7789adc5b0355f2d6976f6e972
Anonnb666 <1760107928@qq.com>
2024-09-23 09:14:46 +0800

    Add files via upload

---
+++ e1.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class e1 {
+
+
+    public static void main(String[] args) {
+        int a=(2+100%7)%7;
+        System.out.println("The "+a+"th day 0f a week");
+    }
+}
